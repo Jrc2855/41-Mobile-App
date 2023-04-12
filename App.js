e2f86ea4ae8b938ffc553295c507c4269eded74c
John Chavez <Jrc2855@gmail.com>
2023-04-12 00:27:53 +0000

    progress

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { ActivityIndicator, Image, StyleSheet, Switch, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { isEnabled } from 'react-native/Libraries/Performance/Systrace';
+import * as Notifications from 'expo-notifications';
+import registerNNPushToken from 'native-notify';
 
 export default function App() {
+  registerNNPushToken(7343, 'kLSLoG1jbixi26v9bDCpbU');
   const [count, setCount] = useState(0);
   const [isEnabled, setIsEnabled] = useState(false);
   const onPress = () => setCount(prevCount => prevCount + 1);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true
+    }),
+  });
+
+  Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'Look at the notification',
+      body: 'You pet the cat!'
+    },
+    trigger: { seconds: 2 },
+  });
 
 
   return (
@@ -29,15 +47,15 @@ export default function App() {
         style={styles.textStyles}
         defaultValue="Hello World"
       />
-      <Switch
+      {/* <Switch
         style={styles.switchStyles}
         trackColor={{ false: '#767577', true: '#74B72E' }}
         ios_backgroundColor='#FF0000'
         onValueChange={toggleSwitch} //I never thought about how this switch has to be coded to stay in the direction it's flipped towards
         value={isEnabled}
-      />
-      <ActivityIndicator />
-      <Text style={styles.freeTextStyles}>The loading indicator doesn't do anything, it's just there to show that I learned about it.</Text>
+      /> */}
+      {/* <ActivityIndicator /> */}
+      {/* <Text style={styles.freeTextStyles}>The loading indicator doesn't do anything, it's just there to show that I learned about it.</Text> */}
     </View>
   );
 }
